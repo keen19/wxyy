7f9db8b641ca170b664187018c220ab434befada
keen19 <775038592+keen-chenxi@users.noreply.github.com>
2020-05-13 16:36:15 +0800

    weixintest

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+//define("TOKEN","keenweixin");
+//
+//$wechatObj=new wechatCallbackapiTest();
+//if(!isset($_GET['echostr'])){
+//    $wechatObj->responseMsg();
+//}else{
+//    $wechatObj->vaild();}
+ echo "hello php"
+ ?>
+
+
